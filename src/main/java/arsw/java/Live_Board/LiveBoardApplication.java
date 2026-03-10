c12b5a5bc0e01b5fa80cfb59a302fa169c0c2c0d
Marlio <marlio.charry-e@mail.escuelaing.edu.co>
2026-03-09 19:03:02 -0500

    Initial commit for spring:boot

---
+++ src/main/java/arsw/java/Live_Board/LiveBoardApplication.java
@@ -0,0 +1,13 @@
+package arsw.java.Live_Board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiveBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/arsw/java/Live_Board/LiveBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package arsw.java.Live_Board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
