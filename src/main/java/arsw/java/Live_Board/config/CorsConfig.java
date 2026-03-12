2360d9a387626e8e526e53cb390ff3cbb59d9062
Marlio <marlio.charry-e@mail.escuelaing.edu.co>
2026-03-12 16:25:01 -0500

    Cors config

--- src/main/java/arsw/java/Live_Board/config/CorsConfig.java
+++ src/main/java/arsw/java/Live_Board/config/CorsConfig.java
@@ -17,7 +17,7 @@ public class CorsConfig {
             .allowedOrigins(
                 "http://localhost:5173",
                 "http://localhost:8080",
-                "https://live-board-fcb6a0eedtdcgfh6.westus3-01.azurewebsites.net")
+                "http://localhost:3000")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
             .allowedHeaders("*");
       }
